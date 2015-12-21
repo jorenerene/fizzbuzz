3560abef52faad12392caf5d864c13291f777e4e
Jorene René <jorenerene@gmail.com>
2015-12-20 23:43:04 -0500

    Minimized to one line.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,10 +3,4 @@ The FizzBuzz Challenge:
 Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"
 */
 
-function fizzbuzz () {
-	for (var x = 1; x <= 100; x ++) {
-		console.log (x % 3 === 0 ? ( x % 5 === 0 ? 'FizzBuzz' : 'Fizz') : (x % 5 === 0 ? 'Buzz' : x));
-	}
-}
-
-fizzbuzz ();
+for (var x = 1; x < 101; x ++) console.log (x % 3 === 0 ? ( x % 5 === 0 ? 'FizzBuzz' : 'Fizz') : (x % 5 === 0 ? 'Buzz' : x));
